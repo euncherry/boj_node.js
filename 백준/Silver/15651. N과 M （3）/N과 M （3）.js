e25860052a6feaf4468010eccf92a7b79cb11785
eunchel <48914971+euncherry@users.noreply.github.com>
2023-07-02 14:44:26 +0900

    [Silver III] Title: N과 M (3), Time: 824 ms, Memory: 138604 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.js
@@ -0,0 +1,36 @@
+//15651.N과M(3).s3
+/**
+ * @method backtracking
+ * @note
+ */
+
+const fs = require("fs");
+
+// const filePath = process.platform === 'linux' ? '/dev/stdin' : 'data.txt';
+const filePath = !process.env.CODESANDBOX_HOST ? "/dev/stdin" : "data.txt";
+
+let inputs = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const N = +inputs[0];
+const M = +inputs[1];
+let answer = "";
+// const isUsed = Array(N + 1);
+const arr = [];
+
+function solution(idx, pre) {
+  if (idx === M) {
+    answer += arr.join(" ");
+    answer += `\n`;
+    return;
+  }
+  for (let i = 1; i <= N; i++) {
+    // if (isUsed[i]) continue;
+    // isUsed[i] = true;
+    arr[idx] = i;
+    solution(idx + 1, i);
+    // isUsed[i] = false;
+  }
+}
+
+solution(0, 0);
+console.log(answer);
