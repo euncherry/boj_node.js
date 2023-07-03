34b64e8cff5111599ec402d507fb76d1780d3b53
eunchel <48914971+euncherry@users.noreply.github.com>
2023-07-03 12:49:17 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.js
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -1,7 +1,7 @@
 //15650.N과M(2).s3
 /**
  * @method backtracking
- * @note
+ * @note [중복없이][오름차순] - for문 초기값 설정
  */
 
 const fs = require("fs");
@@ -14,24 +14,23 @@ let inputs = fs.readFileSync(filePath).toString().trim().split(" ");
 const N = +inputs[0];
 const M = +inputs[1];
 let answer = "";
-const isUsed = Array(N + 1);
+
 const arr = [];
 
-function solution(idx, pre) {
-  if (idx === M) {
+function solution(p) {
+  if (p === M) {
     answer += arr.join(" ");
     answer += `\n`;
     return;
   }
-  for (let i = 1; i <= N; i++) {
-    if (isUsed[i]) continue;
-    if (i < pre) continue;
-    isUsed[i] = true;
-    arr[idx] = i;
-    solution(idx + 1, i);
-    isUsed[i] = false;
+  // 오름차순 -> arr[p-1]보다 큰수만 가능
+  // p가 0일경우 st = 1
+  // p가 0일 경우를 제외한 재귀함수는 arr[p-1]+1(중복x)과 초기값(st)이 같은 for문을 돌면됨
+  for (let st = arr[p - 1] + 1 || 1; st <= N; st++) {
+    // console.log("arr[p-1]", arr[p - 1], `p : ${p} , st :${st}`);
+    arr[p] = st;
+    solution(p + 1);
   }
 }
-
-solution(0, 0);
+solution(0);
 console.log(answer);
