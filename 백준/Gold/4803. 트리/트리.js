154b35c14ef094a14449f353b5e053defbbe54ab
eunchel <48914971+euncherry@users.noreply.github.com>
2023-10-13 03:15:02 +0900

    [Gold IV] Title: 트리, Time: 560 ms, Memory: 52972 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.js
+++ 백준/Gold/4803. 트리/트리.js
@@ -19,29 +19,36 @@ let caseNum = 0;
 for(let i = 0 ; i<inputs.length-1 ; i++){
   caseNum+= 1;
   const [N,M] = inputs[i].split(' ').map(Number);
+  //graph 구현
   const graph = [...Array(N+1)].map((v)=>[...Array()]);
   for(let j = i+1 ; j<=M+i;j++){
     const [f,e]  = inputs[j].split(' ').map(Number);
     graph[f].push(e);
     graph[e].push(f);
   }
+
   answer += `Case ${caseNum}: `
-  hasTree(graph,N);
+  hasTree(graph,N)
   i += M;
 }
 
 
+
 function hasTree(graph,N){
+  // console.log(graph)
+
   let treeCnt = 0;
   const isVis = [];
   let isCycle = false;
+
+  //DFS에서 사이클이 존재한다면 tree가 아님
   function DFS(node , par){
-    isVis[node] = true;
-    for(adj of graph[node]){
-      if(adj === par) continue;
-      if(isVis[adj]) {
-        isCycle = true;
-        continue;
+    isVis[node] = true; // 방문표시
+    for(adj of graph[node]){ // 인접노드확인
+      if(adj === par) continue; // 인접노드가 부모라면 continue;
+      if(isVis[adj]) { //부모가 아닌 인접노드가 존재한다면
+        isCycle = true; // 사이클이 존재함
+        continue; // 연결된 노드를 다 확인 해야 하기 때문에 continue;
       }
       DFS(adj, node)
     }
@@ -50,8 +57,11 @@ function hasTree(graph,N){
     if(isVis[i]) continue;
     isCycle = false;
     DFS(i , 0);
+    //DFS를 돌면서 연결그래프만 나오기 때문에 cycle이 있는지만 확인
     if(!isCycle) treeCnt +=1;
+    // console.log(isCycle);
   }
+    // console.log("---")
 
   if(treeCnt === 0) return answer += `No trees.\n`;
   if(treeCnt === 1) return answer += `There is one tree.\n`;
