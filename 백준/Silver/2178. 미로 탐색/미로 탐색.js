304080dbaf56a265f8072afaefba978800b3346d
eunchel <48914971+euncherry@users.noreply.github.com>
2025-02-18 03:45:44 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,37 +1,46 @@
+//2178.미로탐색.s1
+/**
+ * @입력값 NM <= 10000
+ * @시간 1
+ * @시간복잡도 O(NlogN || N || logN || 1)
+ * @method BFS
+ * @note 이동 거리 vis에 기록
+ */
+
 const fs = require("fs");
 const filePath = !process.env.CODESANDBOX_HOST ? "/dev/stdin" : "data.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [lens, ...arr] = input;
+let [iNum, ...iVal] = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [N, M] = lens.split(" ").map(Number);
+const [N, M] = iNum.split(" ").map(Number);
 
-const inputs = arr.map((v) => v.split("").map(Number));
-const vis = Array.from({ length: N }, (v) => [...Array(M).fill(0)]);
-const news = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
+const board = iVal.map((v) => v.split("").map(Number));
+const vis = Array.from({ length: N }, () => Array(M).fill(0));
 
-function BFS(x, y) {
+function BFS(px, py) {
+  const news = [
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+    [0, 1],
+  ];
   const queue = [];
-  queue.push([x, y]);
-  vis[x][y] = 1;
+  queue.push([px, py]);
+  vis[px][py] = 1;
+
   while (queue.length !== 0) {
-    const [mx, my] = queue.shift();
+    const [dx, dy] = queue.shift();
+    const vLen = vis[dx][dy];
     for (let i = 0; i < 4; i++) {
-      const dx = mx + news[i][0];
-      const dy = my + news[i][1];
-      if (dx < 0 || dy < 0 || dx >= N || dy >= M) continue;
-      if (vis[dx][dy] !== 0 || inputs[dx][dy] === 0) continue;
-      vis[dx][dy] = vis[mx][my] + 1;
-      queue.push([dx, dy]);
+      const mx = news[i][0] + dx;
+      const my = news[i][1] + dy;
+      if (mx < 0 || mx >= N || my < 0 || my >= M) continue;
+      if (board[mx][my] === 0 || vis[mx][my] > 0) continue;
+      queue.push([mx, my]);
+      vis[mx][my] = vLen + 1;
     }
   }
+  return vis[N - 1][M - 1];
 }
 
-BFS(0, 0);
-
-console.log(vis[N - 1][M - 1]);
+console.log(BFS(0, 0));
